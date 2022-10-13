d5e59efd4278c3fd35bc97b45bc79785ef71a78c
Reda Makarem <makarem.reda@gmail.com>
2022-10-13 13:31:01 +0300

    ..

--- assets/theme.js
+++ assets/theme.js
@@ -2268,6 +2268,7 @@
 
           const cleanResponse = element.querySelector('[data-api-content]').innerHTML;
           this.build(cleanResponse);
+          console.log('YAAAS');
         })
         .catch((error) => console.log(error));
     }
